a391af61d3fa862001a1a150030d5a30cb5c20cb
raffe90 <raffe90@gmail.com>
2015-11-22 21:22:24 +0100

    Adds nav

--- resources/views/partials/_header.blade.php
+++ resources/views/partials/_header.blade.php
@@ -1,5 +1,30 @@
 <header class="container-fluid">
-    
+    <div class="row">
+        <div class="col-sm-10 col-sm-offset-1">
+          <div class="row">
+            <div class="col-sm-6">
+              <img src="/img/gestoria-dyd-logo.png" alt="Logo Gestoría D&D" class="pull-left logo">
+            </div>
+            <div class="col-sm-6 header-call-to-action">
+              <div class="row">
+                <div class="col-sm-6">
+                    <span class="header-phone">
+                      LLÁMENOS AL 9100 00 00
+                    </span>
+                    <span class="header-address">
+                      C/ Béjar 13 Madrid
+                    </span>
+                </div>
+                <div class="col-sm-6">
+                    <button class="custom-btn custom-btn-primary">
+                      ÁREA CLIENTES
+                    </button>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+    </div>
     <div class="row">
         @include('partials._nav')
     </div>
---
+++ resources/views/partials/_nav.blade.php
@@ -0,0 +1,28 @@
+<nav>
+  <ul class="nav">
+    <li class="active">
+      <a href="/">inicio</a>
+    </li>
+    <li>
+      <a href="#">empresa</a>
+    </li>
+    <li>
+      <a href="#">tráfico</a>
+    </li>
+    <li>
+      <a href="#">juridico</a>
+    </li>
+    <li>
+      <a href="#">empleada hogar</a>
+    </li>
+    <li>
+      <a href="#">herencia</a>
+    </li>
+    <li>
+      <a href="#">trámites</a>
+    </li>
+    <li>
+      <a href="#">contacto</a>
+    </li>
+  </ul>
+</nav>
